0ee75e0b7ae70cbb8d1fd6fab152592614c346b9
glefriou <lefriouxgaetan@gmail.com>
2018-07-01 23:26:58 +0200

    remove unused file

--- src/modelEvaluator.py
+++
@@ -1,7 +0,0 @@
-import data_loader as dl
-import models.poisson_model as pm
-
-loader = dl.DataLoader()
-poissonModel = pm.PoissonModel(15, 5)
-data = loader.loadFiles(['france1'])
-poissonModel.computeMatchesProbas(data)
